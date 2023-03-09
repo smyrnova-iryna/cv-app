bf915ed9c43ae0edb8f2f4257779afd2feb6554e
Ira1234567 <iraowl1234@gmail.com>
2023-03-09 16:34:17 +0200

    Update contacts styles

--- cv-app/src/components/Contacts.js
+++ cv-app/src/components/Contacts.js
@@ -4,8 +4,10 @@ const Contacts = (props) => {
     return (
         <div>
             <h2 className="sideBarTitle">{props.title}</h2>
-            <p>{props.phoneTitle}<a className="sideBarLink" href='tel:+380676461393'>{props.phoneNumber}</a></p>
-            <p>{props.emailTitle}<a className="sideBarLink" href='mailto:iryna.smyrnova12@gmail.com'>{props.email}</a></p>
+            <p>{props.phoneTitle}</p>
+            <p><a className="sideBarLink" href='tel:+380676461393'>{props.phoneNumber}</a></p>
+            <p>{props.emailTitle}</p>
+            <p><a className="sideBarLink" href='mailto:iryna.smyrnova12@gmail.com'>{props.email}</a></p>
         </div>
     )
 }
